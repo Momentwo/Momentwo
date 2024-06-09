9807924faa50f07b7f87efdf6efbe20430986161
eoeo0326 <eoeo0326@gmail.com>
2024-06-09 21:13:18 +0900

    issue #11 chore: Hilt 설정, Network 권한 추가

--- app/src/main/java/cord/eoeo/momentwo/ui/MainActivity.kt
+++ app/src/main/java/cord/eoeo/momentwo/ui/MainActivity.kt
@@ -4,7 +4,9 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import cord.eoeo.momentwo.ui.theme.MomentwoTheme
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)

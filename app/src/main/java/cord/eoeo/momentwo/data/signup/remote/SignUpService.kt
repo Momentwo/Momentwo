3e0a84a48e29e45797c7c83e589ef5de5cf9f4c4
eoeo0326 <eoeo0326@gmail.com>
2024-07-05 23:17:56 +0900

    issue #16 feat: Email, Nickname 중복 확인 service 구현, URL 추가

--- app/src/main/java/cord/eoeo/momentwo/data/signup/remote/SignUpService.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/signup/remote/SignUpService.kt
@@ -1,7 +1,10 @@
 package cord.eoeo.momentwo.data.signup.remote
 
 import cord.eoeo.momentwo.data.MomentwoApi
+import cord.eoeo.momentwo.data.model.Email
+import cord.eoeo.momentwo.data.model.Nickname
 import cord.eoeo.momentwo.data.model.User
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.Headers
 import retrofit2.http.POST
@@ -11,5 +14,17 @@ interface SignUpService {
     @POST(MomentwoApi.POST_SIGN_UP)
     suspend fun postSignUp(
         @Body user: User,
-    ): Unit
+    )
+
+    @Headers("content-type: application/json")
+    @POST(MomentwoApi.POST_CHECK_EMAIL)
+    suspend fun postCheckEmail(
+        @Body email: Email,
+    ): Response<Unit>
+
+    @Headers("content-type: application/json")
+    @POST(MomentwoApi.POST_CHECK_NICKNAME)
+    suspend fun postCheckNickname(
+        @Body nickname: Nickname,
+    ): Response<Unit>
 }

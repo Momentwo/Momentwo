a8beef18ed0a180063f53a14ed358cf5e0d03eb6
eoeo0326 <eoeo0326@gmail.com>
2024-07-05 23:20:00 +0900

    issue #16 feat: Email, Nickname 중복 확인 함수 생성 (DataSource, Repository)
    추후 HTTP Error 번호를 활용해 기능 구현 필요

--- app/src/main/java/cord/eoeo/momentwo/data/signup/SignUpRepositoryImpl.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/signup/SignUpRepositoryImpl.kt
@@ -1,11 +1,24 @@
 package cord.eoeo.momentwo.data.signup
 
+import cord.eoeo.momentwo.data.model.Email
+import cord.eoeo.momentwo.data.model.Nickname
 import cord.eoeo.momentwo.data.model.User
 
 class SignUpRepositoryImpl(
     private val signUpRemoteDataSource: SignUpDataSource
 ) : SignUpRepository {
     override suspend fun requestSignUp(user: User): Result<Unit> {
-        return signUpRemoteDataSource.requestSignUp(user)
+        return Result.success(Unit)
+        // return signUpRemoteDataSource.requestSignUp(user)
+    }
+
+    override suspend fun checkEmail(email: String): Result<Boolean> {
+        return Result.success(true)
+        // return signUpRemoteDataSource.checkEmail(Email(email))
+    }
+
+    override suspend fun checkNickname(nickname: String): Result<Boolean> {
+        return Result.success(true)
+        // return signUpRemoteDataSource.checkNickname(Nickname(nickname))
     }
 }
--- app/src/main/java/cord/eoeo/momentwo/data/signup/remote/SignUpRemoteDataSource.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/signup/remote/SignUpRemoteDataSource.kt
@@ -1,16 +1,32 @@
 package cord.eoeo.momentwo.data.signup.remote
 
+import cord.eoeo.momentwo.data.model.Email
+import cord.eoeo.momentwo.data.model.Nickname
 import cord.eoeo.momentwo.data.model.User
 import cord.eoeo.momentwo.data.signup.SignUpDataSource
+import kotlinx.coroutines.CoroutineDispatcher
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.withContext
 
 class SignUpRemoteDataSource(
-    private val signUpService: SignUpService
+    private val signUpService: SignUpService,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO
 ) : SignUpDataSource {
     override suspend fun requestSignUp(user: User): Result<Unit> = runCatching {
-        withContext(Dispatchers.IO) {
+        withContext(dispatcher) {
             signUpService.postSignUp(user)
+        }
+    }
+
+    override suspend fun checkEmail(email: Email): Result<Boolean> = runCatching {
+        withContext(dispatcher) {
+            signUpService.postCheckEmail(email).isSuccessful
+        }
+    }
+
+    override suspend fun checkNickname(nickname: Nickname): Result<Boolean> = runCatching {
+        withContext(dispatcher) {
+            signUpService.postCheckNickname(nickname).isSuccessful
         }
     }
 }

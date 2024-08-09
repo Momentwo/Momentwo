c87854489962935bcf77ae5527e34e266eeecb86
eoeo0326 <eoeo0326@gmail.com>
2024-08-09 16:33:03 +0900

    issue #27 feat: 친구 목록, 요청 목록 Item Composable 구현

---
+++ app/src/main/java/cord/eoeo/momentwo/ui/friend/composable/FriendItem.kt
@@ -0,0 +1,45 @@
+package cord.eoeo.momentwo.ui.friend.composable
+
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.aspectRatio
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AccountCircle
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import cord.eoeo.momentwo.ui.model.UserItem
+
+@Composable
+fun FriendItem(item: () -> UserItem) {
+    Row(
+        modifier =
+            Modifier
+                .height(60.dp)
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp, vertical = 8.dp),
+    ) {
+        Icon(
+            imageVector = Icons.Default.AccountCircle,
+            contentDescription = "",
+            modifier = Modifier.fillMaxHeight().aspectRatio(1f),
+        )
+        Text(
+            text = item().nickname,
+            fontSize = 16.sp,
+            modifier =
+                Modifier
+                    .fillMaxHeight()
+                    .wrapContentHeight(align = Alignment.CenterVertically)
+                    .padding(start = 8.dp),
+        )
+    }
+}

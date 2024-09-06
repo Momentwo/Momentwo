a199d311af4e8765930e212f9c71784ad7778967
eoeo0326 <eoeo0326@gmail.com>
2024-09-06 15:15:26 +0900

    issue #40 feat: 친구 기능 DI - Hilt Module 구현

---
+++ app/src/main/java/cord/eoeo/momentwo/di/FriendModule.kt
@@ -0,0 +1,29 @@
+package cord.eoeo.momentwo.di
+
+import cord.eoeo.momentwo.data.friend.FriendDataSource
+import cord.eoeo.momentwo.data.friend.FriendRepository
+import cord.eoeo.momentwo.data.friend.FriendRepositoryImpl
+import cord.eoeo.momentwo.data.friend.remote.FriendRemoteDataSource
+import cord.eoeo.momentwo.data.friend.remote.FriendService
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object FriendModule {
+    @Provides
+    @Singleton
+    fun provideFriendService(retrofit: Retrofit): FriendService = retrofit.create(FriendService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideFriendRemoteDataSource(friendService: FriendService): FriendDataSource = FriendRemoteDataSource(friendService)
+
+    @Provides
+    @Singleton
+    fun provideFriendRepository(friendRemoteDataSource: FriendDataSource): FriendRepository = FriendRepositoryImpl(friendRemoteDataSource)
+}

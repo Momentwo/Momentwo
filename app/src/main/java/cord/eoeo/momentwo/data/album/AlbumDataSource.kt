b372510753353674a67facf97795cecaf717d4e7
eoeo0326 <eoeo0326@gmail.com>
2024-09-09 16:37:18 +0900

    issue #36 feat: AlbumDataSource 구현

---
+++ app/src/main/java/cord/eoeo/momentwo/data/album/AlbumDataSource.kt
@@ -0,0 +1,25 @@
+package cord.eoeo.momentwo.data.album
+
+import cord.eoeo.momentwo.data.model.AlbumImage
+import cord.eoeo.momentwo.data.model.AlbumSubTitle
+import cord.eoeo.momentwo.data.model.AlbumTitle
+import cord.eoeo.momentwo.data.model.CreateAlbumInfo
+
+interface AlbumDataSource {
+    suspend fun requestCreateAlbum(createAlbumInfo: CreateAlbumInfo): Result<Unit>
+
+    suspend fun deleteAlbum(albumId: Int): Result<Unit>
+
+    suspend fun changeAlbumImage(albumImage: AlbumImage): Result<Unit>
+
+    suspend fun deleteAlbumImage(albumId: Int): Result<Unit>
+
+    suspend fun changeAlbumSubTitle(albumSubTitle: AlbumSubTitle): Result<Unit>
+
+    suspend fun deleteAlbumSubTitle(albumId: Int): Result<Unit>
+
+    suspend fun changeAlbumTitle(
+        albumId: Int,
+        albumTitle: AlbumTitle,
+    ): Result<Unit>
+}
---
+++ app/src/main/java/cord/eoeo/momentwo/data/album/remote/AlbumRemoteDataSource.kt
@@ -0,0 +1,67 @@
+package cord.eoeo.momentwo.data.album.remote
+
+import cord.eoeo.momentwo.data.album.AlbumDataSource
+import cord.eoeo.momentwo.data.model.AlbumImage
+import cord.eoeo.momentwo.data.model.AlbumSubTitle
+import cord.eoeo.momentwo.data.model.AlbumTitle
+import cord.eoeo.momentwo.data.model.CreateAlbumInfo
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+
+class AlbumRemoteDataSource(
+    private val albumService: AlbumService,
+    private val dispatcher: CoroutineDispatcher = Dispatchers.IO,
+) : AlbumDataSource {
+    override suspend fun requestCreateAlbum(createAlbumInfo: CreateAlbumInfo): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.postCreateAlbum(createAlbumInfo)
+            }
+        }
+
+    override suspend fun deleteAlbum(albumId: Int): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.deleteAlbum(albumId)
+            }
+        }
+
+    override suspend fun changeAlbumImage(albumImage: AlbumImage): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.putAlbumImage(albumImage)
+            }
+        }
+
+    override suspend fun deleteAlbumImage(albumId: Int): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.deleteAlbumImage(albumId)
+            }
+        }
+
+    override suspend fun changeAlbumSubTitle(albumSubTitle: AlbumSubTitle): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.putAlbumSubTitle(albumSubTitle)
+            }
+        }
+
+    override suspend fun deleteAlbumSubTitle(albumId: Int): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.deleteAlbumSubTitle(albumId)
+            }
+        }
+
+    override suspend fun changeAlbumTitle(
+        albumId: Int,
+        albumTitle: AlbumTitle,
+    ): Result<Unit> =
+        runCatching {
+            withContext(dispatcher) {
+                albumService.putAlbumTitle(albumId, albumTitle)
+            }
+        }
+}

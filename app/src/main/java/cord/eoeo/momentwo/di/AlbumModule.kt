644ccb2fe6370208de92db9865cefdd1ef3a8bab
eoeo0326 <eoeo0326@gmail.com>
2024-09-09 16:45:24 +0900

    issue #36 feat: DI - Hilt AlbumModule 구현

---
+++ app/src/main/java/cord/eoeo/momentwo/di/AlbumModule.kt
@@ -0,0 +1,35 @@
+package cord.eoeo.momentwo.di
+
+import cord.eoeo.momentwo.data.album.AlbumDataSource
+import cord.eoeo.momentwo.data.album.AlbumRepositoryImpl
+import cord.eoeo.momentwo.data.album.remote.AlbumRemoteDataSource
+import cord.eoeo.momentwo.data.album.remote.AlbumService
+import cord.eoeo.momentwo.domain.album.AlbumRepository
+import cord.eoeo.momentwo.domain.album.RequestCreateAlbumUseCase
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+object AlbumModule {
+    @Provides
+    @Singleton
+    fun provideAlbumService(retrofit: Retrofit): AlbumService = retrofit.create(AlbumService::class.java)
+
+    @Provides
+    @Singleton
+    fun provideAlbumRemoteDataSource(albumService: AlbumService): AlbumDataSource = AlbumRemoteDataSource(albumService)
+
+    @Provides
+    @Singleton
+    fun provideAlbumRepository(albumRemoteDataSource: AlbumDataSource): AlbumRepository = AlbumRepositoryImpl(albumRemoteDataSource)
+
+    @Provides
+    @Singleton
+    fun provideGetFriendListUseCase(albumRepository: AlbumRepository): RequestCreateAlbumUseCase =
+        RequestCreateAlbumUseCase(albumRepository)
+}

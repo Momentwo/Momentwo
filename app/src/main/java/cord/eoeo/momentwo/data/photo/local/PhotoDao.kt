95358a282188f2b39d809f7d86e6e3cd1ee4c5cc
eoeo0326 <eoeo0326@gmail.com>
2024-10-18 00:40:54 +0900

    issue #71 feat: PhotoEntity, Dao 수정

--- app/src/main/java/cord/eoeo/momentwo/data/photo/local/PhotoDao.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/photo/local/PhotoDao.kt
@@ -1,21 +1,21 @@
 package cord.eoeo.momentwo.data.photo.local
 
+import androidx.paging.PagingSource
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
+import cord.eoeo.momentwo.data.photo.local.entity.PhotoEntity
 
 @Dao
 interface PhotoDao {
-    @Query("SELECT * FROM photo WHERE album_id = :albumId AND sub_album_id = :subAlbumId")
-    fun getPhotoList(albumId: Int, subAlbumId: Int): List<PhotoEntity>
+    @Query("SELECT * FROM photo WHERE album_id = :albumId AND sub_album_id = :subAlbumId ORDER BY id")
+    fun getPhotoPagingSource(albumId: Int, subAlbumId: Int): PagingSource<Int, PhotoEntity>
 
-    @Query("SELECT * FROM photo WHERE id = :photoId LIMIT 1")
-    fun findById(photoId: Int): PhotoEntity
-
-    @Insert
-    fun insertALl(vararg photos: PhotoEntity)
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(vararg photos: PhotoEntity)
 
     @Delete
-    fun delete(photo: PhotoEntity)
+    suspend fun deleteAll(vararg photo: PhotoEntity)
 }
--- app/src/main/java/cord/eoeo/momentwo/data/photo/local/entity/PhotoEntity.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/photo/local/entity/PhotoEntity.kt
@@ -1,8 +1,9 @@
-package cord.eoeo.momentwo.data.photo.local
+package cord.eoeo.momentwo.data.photo.local.entity
 
 import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.PrimaryKey
+import cord.eoeo.momentwo.ui.model.ImageItem
 
 @Entity(tableName = "photo")
 data class PhotoEntity(
@@ -10,4 +11,10 @@ data class PhotoEntity(
     @ColumnInfo(name = "album_id") val albumId: Int,
     @ColumnInfo(name = "sub_album_id") val subAlbumId: Int,
     @ColumnInfo(name = "image_url") val imageUrl: String,
-)
+) {
+    fun mapToImageItem(): ImageItem =
+        ImageItem(
+            id = id,
+            imageUrl = imageUrl,
+        )
+}

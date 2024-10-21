71b2763bf10a6d46ec707665cf9396ff17a7e38f
eoeo0326 <eoeo0326@gmail.com>
2024-10-22 00:34:48 +0900

    issue #75 feat: 로그인 요청 - Token Json -> Header 변경사항 반영

--- app/src/main/java/cord/eoeo/momentwo/data/login/LoginRepository.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/login/LoginRepository.kt
@@ -1,7 +1,7 @@
 package cord.eoeo.momentwo.data.login
 
-import cord.eoeo.momentwo.data.model.LoginResponse
+import cord.eoeo.momentwo.ui.model.TokenItem
 
 interface LoginRepository {
-    suspend fun requestLogin(email: String, password: String): Result<LoginResponse>
+    suspend fun requestLogin(email: String, password: String): Result<TokenItem>
 }

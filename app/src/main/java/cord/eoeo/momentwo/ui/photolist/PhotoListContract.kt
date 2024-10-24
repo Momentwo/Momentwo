fc34ecf0b560979420f210fed572d05f73c75c35
eoeo0326 <eoeo0326@gmail.com>
2024-10-24 20:05:15 +0900

    issue #68 feat: PhotoListContract, ViewModel 수정

--- app/src/main/java/cord/eoeo/momentwo/ui/photolist/PhotoListContract.kt
+++ app/src/main/java/cord/eoeo/momentwo/ui/photolist/PhotoListContract.kt
@@ -1,5 +1,6 @@
 package cord.eoeo.momentwo.ui.photolist
 
+import android.net.Uri
 import androidx.paging.PagingData
 import cord.eoeo.momentwo.ui.UiEffect
 import cord.eoeo.momentwo.ui.UiEvent
@@ -15,6 +16,8 @@ class PhotoListContract {
         val albumTitle: String = "",
         val subAlbumTitle: String = "",
         val photoPagingData: Flow<PagingData<ImageItem>> = emptyFlow(),
+        val selectedPhotoIds: List<Int> = emptyList(),
+        val selectedPhotoUrls: List<String> = emptyList(),
         val isMenuExpended: Boolean = false,
         val isEditMode: Boolean = false,
         val isDialogOpened: Boolean = false,
@@ -25,9 +28,13 @@ class PhotoListContract {
     ) : UiState
 
     sealed interface Event : UiEvent {
+        data class OnUploadImage(val imageUri: Uri, val mimeType: String) : Event
         data class OnChangeIsRefreshing(val isRefreshing: Boolean) : Event
         data class OnChangeIsMenuExpended(val isMenuExpended: Boolean) : Event
         data class OnChangeIsEditMode(val isEditMode: Boolean) : Event
+        data object OnClickCancelEdit : Event
+        data object OnClickConfirmEdit : Event
+        data class OnChangeIsSelected(val isSelected: Boolean, val imageItem: ImageItem) : Event
         data class OnChangeIsDialogOpened(val isDialogOpened: Boolean) : Event
         data class OnConfirmDialog(val subAlbumTitle: String) : Event
         data object OnBack : Event

bdc94328d323f3c1b4833a65d6e2e5617054c318
eoeo0326 <eoeo0326@gmail.com>
2024-10-24 20:02:52 +0900

    issue #68 feat: PhotoRepository 추가 기능 구현, Hilt PhotoModule 수정

--- app/src/main/java/cord/eoeo/momentwo/data/photo/PhotoRepository.kt
+++ app/src/main/java/cord/eoeo/momentwo/data/photo/PhotoRepository.kt
@@ -1,9 +1,14 @@
 package cord.eoeo.momentwo.data.photo
 
+import android.net.Uri
 import androidx.paging.PagingData
 import cord.eoeo.momentwo.ui.model.ImageItem
 import kotlinx.coroutines.flow.Flow
 
 interface PhotoRepository {
     suspend fun getPhotoPagingData(pageSize: Int, albumId: Int, subAlbumId: Int): Flow<PagingData<ImageItem>>
+
+    suspend fun requestUploadPhoto(albumId: Int, subAlbumId: Int, mimeType: String, image: Uri): Result<Unit>
+
+    suspend fun deletePhotos(albumId: Int, subAlbumId: Int, imageIds: List<Int>, imageUrls: List<String>): Result<Unit>
 }

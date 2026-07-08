fc3c1bee1f7313ca1d9062a0d5a7202013a5f962
eoeo0326 <eoeo0326@gmail.com>
2026-07-08 18:38:00 +0900

    issue #117 chore: :core:designsystem 모듈 생성 및 settings 등록

---
+++ core/designsystem/build.gradle.kts
@@ -0,0 +1,8 @@
+plugins {
+    alias(libs.plugins.momentwo.android.library)
+    alias(libs.plugins.momentwo.android.library.compose)
+}
+
+android {
+    namespace = "cord.eoeo.momentwo.core.designsystem"
+}

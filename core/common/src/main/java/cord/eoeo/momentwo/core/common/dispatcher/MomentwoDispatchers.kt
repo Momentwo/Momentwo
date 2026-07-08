8eb95536c94a19a462fbf614c3259c33182fe54d
eoeo0326 <eoeo0326@gmail.com>
2026-07-08 19:07:26 +0900

    issue #112 feat: Coroutine Dispatcher 한정자 및 DispatchersModule 추가
    @IoDispatcher/@DefaultDispatcher/@MainDispatcher 한정자와 이를 제공하는
    DispatchersModule(SingletonComponent)을 :core:common 에 신설한다.
    데이터 소스/레포의 하드코딩된 Dispatchers.IO 를 주입으로 전환하는 작업은
    해당 파일이 :core:data 로 이동하는 PR에서 이동과 함께 처리한다.

---
+++ core/common/src/main/java/cord/eoeo/momentwo/core/common/dispatcher/MomentwoDispatchers.kt
@@ -0,0 +1,15 @@
+package cord.eoeo.momentwo.core.common.dispatcher
+
+import javax.inject.Qualifier
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class IoDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class DefaultDispatcher
+
+@Qualifier
+@Retention(AnnotationRetention.BINARY)
+annotation class MainDispatcher
